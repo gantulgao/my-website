19838a27a5380aaafa02e60497b8e95e942690ef
banzaa <127931165+bzscma@users.noreply.github.com>
2023-07-05 04:43:59 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,10 @@
-// @ts-check
+/ @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
+/** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "EFinance guide",
   tagline: "Dinosaurs are cool",
@@ -34,8 +34,9 @@ const config = {
 
   presets: [
     [
-      "@docusaurus/preset-classic",
-      {
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
@@ -43,55 +44,50 @@ const config = {
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-      },
+      }),
     ],
   ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      title: "EFinance Guide",
-      logo: {
-        alt: "EFinance Logo",
-        src: "img/logo.svg",
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "EFinance Guide",
+        logo: {
+          alt: "EFinance Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
+        ],
       },
-      items: [
-        {
-          type: "doc",
-          docId: "Tanilcuulga",
-          position: "left",
-          label: "Tutorial",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/Tanilcuulga",
-            },
-          ],
-        },
-      ],
-      // Update the copyright information
-      // based on your preferences
-      // The current format is just a placeholder
-      // Feel free to change it
-      copyright:
-        "Copyright © " +
-        new Date().getFullYear() +
-        " EFinance Guide, iSolvers LLC",
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/Tanilcuulga",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} EFinance Guide, iSolvers LLC`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
