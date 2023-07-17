eff8d05fd59b5b8e667fdf9c1d706699fd16e5e3
Sambuu <84826740+aSambuu@users.noreply.github.com>
2023-07-17 03:17:34 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "EFINE санхүүгийн суурь систем",
+  title: "EFINE суурь систем",
   tagline: "",
   favicon: "img/favicon.ico",
 
