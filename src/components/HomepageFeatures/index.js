bc66513f67953beab93aa482d7a8f9c19ed2fea9
Sambuu <84826740+aSambuu@users.noreply.github.com>
2023-07-18 10:23:32 +0000

    Update 4 files

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,27 +5,28 @@ import styles from "./styles.module.css";
 const FeatureList = [
   {
     title: "Клауд технологид суурилсан ",
-    Svg: require("@site/static/img/hh77.svg").default,
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Клауд веб технологид суурилсан тул та интернет холбогдсон бүх төрлийн
+        Клауд веб технологид суурилсан тул та интернэт холбогдсон бүх төрлийн
         төхөөрөмжөөс системд хандах боломжтой.
       </>
     ),
   },
   {
-    title: "Зээлийн үйл ажиллагаа автомажсан",
-    Svg: require("@site/static/img/gg5.svg").default,
+    title: "Зээлийн үйл ажиллагаа автоматжуулалт",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Зээлийн үйл ажиллагааг ажлын урсгалд суурилсан байдлаар автомажуулж гар
+        Зээлийн үйл ажиллагааг ажлын урсгалд суурилсан байдлаар автоматжуулж гар
         ажиллагааг багасгаж хялбаршуулсан.
       </>
     ),
   },
   {
     title: "Санхүүгийн найдвартай тооцоолол",
-    Svg: require("@site/static/img/gg2.svg").default,
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+
     description: (
       <>
         Байгууллагын бүхий л санхүүгийн гүйлгээг хийснээр бүх тооцоолол, тайлан
