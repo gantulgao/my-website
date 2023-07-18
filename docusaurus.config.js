4436ad1fb8867c18c86265d96f6431d35d040a56
Sambuu <84826740+aSambuu@users.noreply.github.com>
2023-07-18 05:17:35 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "EFINE суурь систем",
   tagline: "",
-  favicon: "img/favicon.ico",
+  favicon: "static/img/efine 1.png",
 
   // Set the production url of your site here
   url: "http://docs.efine.mn",
@@ -48,7 +48,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "static/img/efine 1.png",
       navbar: {
         title: "efine",
         logo: {
