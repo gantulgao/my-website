fa47c873f74fd7c3b66f3ea535738fe566be95e3
Sambuu <84826740+aSambuu@users.noreply.github.com>
2023-07-18 05:08:47 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/nevtreh"
           >
-            EFinance guide ⏱️
+            Гарын авлага ⏱️
           </Link>
         </div>
       </div>
